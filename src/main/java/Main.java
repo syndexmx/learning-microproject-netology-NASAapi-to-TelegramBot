40d6a5f557627aa7cb3486d2a4ea164bd755e4c3
Maxim Shapovalov <syndex@list.ru>
2024-01-11 11:03:24 +0300

    upload to GitHub and *.ini properties loading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,26 +2,26 @@ import nasaservice.NasaAPIAuth;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import telegrambotcontroller.TelegramBotController;
 
-import javax.swing.*;
+import java.io.*;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, TelegramApiException {
-        String  telegramBotsToken;
+        String telegramBotsToken;
         String telegramBotsName;
         String nasaToken;
-        if (args.length==0){
-            telegramBotsName = JOptionPane.showInputDialog("Enter bot's Name");
-            telegramBotsToken = JOptionPane.showInputDialog("Enter bot's Token");
-           nasaToken = JOptionPane.showInputDialog("Enter NASA Token");
-        } else {
-            telegramBotsToken = args[0];
-            telegramBotsName = args[1];
-            nasaToken = args[2];
+
+        BufferedReader bReader = new BufferedReader(new FileReader ("telegramandnasa.ini"));
+        try {
+            telegramBotsName = bReader.readLine();
+            telegramBotsToken = bReader.readLine();
+            nasaToken = bReader.readLine();
+        } finally {
+            bReader.close();
         }
+
         NasaAPIAuth.NASA_API_TOKEN = nasaToken;
         new TelegramBotController(telegramBotsName,telegramBotsToken);
-
 
     }
 }
